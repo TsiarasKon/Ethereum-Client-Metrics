8108efb29042cbd6387c5213192ab815c0463520
ktsiaras <ktsiaras@netcompany-intrasoft.com>
2023-02-05 21:50:19 +0200

    Handling client==ALL in graphs

--- generate_configs_graphs.py
+++ generate_configs_graphs.py
@@ -18,20 +18,27 @@ def hours_formatter(seconds, pos=None):
     return str(h)
 
 
-def plot_col_multidf(config, df_list, col, title, ylabel):
+def plot_col_multidf(config, df_list, col, title, ylabel, client):
     longest_df = max(df_list, key=lambda x: len(x))
     fig, ax = plt.subplots(constrained_layout=True)
-    fig.set_size_inches(7, 5)
+    fig.set_size_inches(7, 5.25)
     fig.set_dpi(100)
     ax.xaxis.set_major_formatter(hours_formatter)
     ax.set_xticks(
         [sec for sec in longest_df.index[::config['time_ticks_step']]])
+    events_set = []     # used only if client == 'ALL'
     for run, df in zip(config['runs'], df_list):
         ax.plot(df.index, df[col], label=run['name'], zorder=5)
         for event in run['events']:
             x_index = event['x'] // INTERVAL
-            marker_label = event['config']['name'] if run == config['runs'][-1] else "_nolegend_"
-            # TODO: labels if client == ALL?
+            if client == "ALL":
+                if event['config']['marker'] in events_set:
+                    marker_label = "_nolegend_"
+                elif client == "ALL":
+                    marker_label = event['config']['name']
+                    events_set.append(event['config']['marker'])
+            else:
+                marker_label = event['config']['name'] if run == config['runs'][-1] else "_nolegend_"
             ax.scatter(df.index[x_index], df.iloc[x_index][col], marker=event['config']['marker'],
                        color=event['config']['color'], s=event['config']['size'], label=marker_label, zorder=10)
 
@@ -39,7 +46,12 @@ def plot_col_multidf(config, df_list, col, title, ylabel):
     plt.ylabel(ylabel)
     plt.xlabel("Time (hours)")
     plt.grid('on', linestyle='--')
-    plt.legend()
+    if client == "ALL":
+        handles, labels = plt.gca().get_legend_handles_labels()
+        order = [0, 5, 7, 1, 2, 3, 4, 6]
+        plt.legend([handles[i] for i in order], [labels[i] for i in order])
+    else:
+        plt.legend()
     # plt.savefig(f"{client}_{col}.png")
     plt.savefig(f"{col}.png")
     # plt.show()
@@ -67,16 +79,16 @@ sys_memory = psutil.virtual_memory().total
 df_list = list(map(lambda run_config: load_df(run_config), config['runs']))
 
 # Plots
-# sns.set()   # TODO - keep?
-plot_col_multidf(config, df_list, 'CPU', "CPU usage over time", "%")
-plot_col_multidf(config, df_list, 'MEM', "RAM usage over time", "GB")
+sns.set()   # TODO - keep?
+plot_col_multidf(config, df_list, 'CPU', "CPU usage over time", "%", client)
+plot_col_multidf(config, df_list, 'MEM', "RAM usage over time", "GB", client)
 plot_col_multidf(config, df_list, 'Disk',
-                 "Chain data disk size over time", "GB")
+                 "Chain data disk size over time", "GB", client)
 plot_col_multidf(config, df_list, 'Reads',
-                 "Disk Reads over time", "MB per second")
+                 "Disk Reads over time", "MB per second", client)
 plot_col_multidf(config, df_list, 'Writes',
-                 "Disk Writes over time", "MB per second")
+                 "Disk Writes over time", "MB per second", client)
 plot_col_multidf(config, df_list, 'Sent',
-                 "Network - Sent data over time", "MB per second")
+                 "Network - Sent data over time", "MB per second", client)
 plot_col_multidf(config, df_list, 'Received',
-                 "Network - Received data over time", "MB per second")
+                 "Network - Received data over time", "MB per second", client)
